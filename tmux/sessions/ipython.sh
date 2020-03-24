4392fd5a28d58a148ce28b2e7818cf8e311109a1
adibl <bleyer23@gmail.com>
2020-03-24 12:19:07 +0200

    c debug func + python pydoc, add ipython env

---
+++ tmux/sessions/ipython.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SESSION="ipyton"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+START_DIR='/home/adi/usb/ipython'
+
+cd "$START_DIR"
+# Only create tmux session if it doesn't already exist
+if [ "$SESSIONEXISTS" = "" ]
+then
+    tmux new-session -c $START_DIR -d -s $SESSION
+    tmux split-window -v -p 20
+
+    tmux select-pane -t $SESSION:1.1
+    tmux send-keys "vim" C-m
+    tmux send-key ":Exp" C-m
+
+    tmux select-pane -t $SESSION:1.2
+
+    tmux select-window -t $SESSION:1
+    tmux attach-session -t $SESSION 
+else
+    tmux attach-session -t $SESSION
+fi
